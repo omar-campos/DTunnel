4ad4d631daf06829318121b3ce9c538a933b98c6
omar-campos <95669004+omar-campos@users.noreply.github.com>
2026-07-23 11:08:27 -0600

    Update config.js

--- frontend/public/static/js/app_config/dialog/config.js
+++ frontend/public/static/js/app_config/dialog/config.js
@@ -2,12 +2,12 @@ import AbsDialog from "./dialog.js";
 
 export default class DialogConfig extends AbsDialog {
     render() {
-        this.dialogHeader.setTitle('DIALOG DE CONFIGURAÇÃO');
+        this.dialogHeader.setTitle('DIÁLOGO DE CONFIGURACIÓN');
         this.dialogHeader.setCloseButton(e => {
             e.stopPropagation();
             this.close();
         });
-        this.dialogContent.element.innerText = 'DADOS DE CONFIGURAÇÃO';
+        this.dialogContent.element.innerText = 'DATOS DE CONFIGURACIÓN';
         this.dialog.setStyle({ width: '350px'});
         super.render();
     }
